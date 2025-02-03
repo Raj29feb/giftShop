1d91c9c160986dce3b9f1b5992722b23e0d3700d
RajAgarwal <raj.agarwal@remotestate.com>
2025-02-03 16:27:17 +0530

    Done with filters - not persisting

--- src/app/api-service.service.ts
+++ src/app/api-service.service.ts
@@ -1,13 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  Observable,
+  switchMap,
+} from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiServiceService {
   constructor(private http: HttpClient) {}
-  getProducts(url: string) {
-    //use pipe and return only the latest request.
-    return this.http.get(url);
+  getProducts(url$: Observable<string>): Observable<any> {
+    console.log('changed url::', url$);
+    return url$.pipe(
+      switchMap((newUrl) => {
+        console.log(newUrl);
+        return this.http.get(newUrl);
+      })
+    );
   }
 }
--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -15,6 +15,7 @@ import { FooterComponent } from '../../Components/footer/footer.component';
 import { PageEvent } from '@angular/material/paginator';
 import { FormsModule } from '@angular/forms';
 import { MatSlider } from '@angular/material/slider';
+import { Observable, of, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -31,6 +32,8 @@ import { MatSlider } from '@angular/material/slider';
 })
 export class HomeComponent implements OnInit {
   @ViewChild('rangeSlider') rangeSlider!: MatSlider; // Access the slider
+  selectedSort: string = 'price-asc';
+
   data = [];
   filteredData = this.data;
   totalNumberOfProducts = 0;
@@ -50,17 +53,23 @@ export class HomeComponent implements OnInit {
   encryptedGiftWrappingValue = signal<string>('');
   giftWrappingIds: Array<string> = [];
   interestFilters: Array<string> = [];
+  encryptedInterestFilters = signal<string>('');
+  encryptedSocialInterestFilters = signal<string>('');
   interestFiltersIds: Array<string> = [];
   socialInterestFiltersIds: Array<string> = [];
   socialInterestsFilters: Array<string> = [];
   colorFilters: Array<string> = [];
+  encryptedColorFilters = signal<string>('');
   colorFiltersIds: Array<string> = [];
   //our product filter checkbox
   ourPrd = signal(false);
   //initial current page
-  page = signal(1);
+  page = signal(0);
   section = false;
   checkSection = signal(this.section);
+
+  order = signal<string>('');
+  orderby = signal<string>('');
 
   //using angular mui for pagination
 
@@ -167,6 +176,37 @@ export class HomeComponent implements OnInit {
     this.ourPrd.set(!this.ourPrd());
   }
 
+  selectSortBy(sortOption: string) {
+    this.selectedSort = sortOption;
+    // Use the selectedSort value to update your data or make API calls
+    switch (this.selectedSort.toLowerCase()) {
+      case 'price-asc':
+        this.order.set('ASC');
+        this.orderby.set('price');
+        break;
+      case 'price-desc':
+        this.order.set('DESC');
+        this.orderby.set('price');
+        break;
+      case 'newest':
+        this.order.set('');
+        this.orderby.set('created_at');
+        break;
+      case 'marketplace':
+        this.order.set('');
+        this.orderby.set('toandfrom');
+        break;
+      case 'trending':
+        this.order.set('');
+        this.orderby.set('trending_order');
+        break;
+      default:
+        this.order.set('');
+        this.orderby.set('');
+        break;
+    }
+  }
+
   handleSection() {
     this.checkSection.set(this.section);
   }
@@ -189,15 +229,160 @@ export class HomeComponent implements OnInit {
   }
 
   //get encrypted giftWrapping values
-  getGiftWrappingEncryptedValue(giftWrapping: string): void {
+  getGiftWrappingEncryptedValue(giftWrapping: string): string {
     switch (giftWrapping.toLowerCase()) {
-      case 'To&From Signature Wrapping':
-        this.encryptedGiftWrappingValue.set(
-          '59ef0239-67c0-40b2-b61f-936758985b9b'
-        );
+      case 'to&from signature wrapping':
+        return 'c136a506-7cf6-4e9e-a4dd-42cc8d78a023';
         break;
       default:
-        this.encryptedGiftWrappingValue.set('');
+        return '';
+    }
+  }
+
+  //get encrypted giftWrapping values
+  getEncryptedInterestFilters(interests: string): string {
+    switch (interests.toLowerCase()) {
+      case 'handbags, wallets & accessories':
+        return '54bbb248-46fe-41f5-97ec-e1ee5214b078';
+        break;
+      case 'botanicals':
+        return 'ec949467-9ae8-45ef-b8c9-765cd69ff105';
+        break;
+      case 'jewelry':
+        return '62a544a7-8baa-4241-9994-809c61b618dd';
+        break;
+      case 'something for the home':
+        return '7327fb43-4bc6-48af-bd34-7bed298d5c28';
+        break;
+      case 'outdoor fun':
+        return 'f96f7ca0-1710-4eae-b23a-c0bac20f2be4';
+        break;
+      case 'personalized gifts':
+        return '69a1ddaa-4159-4175-a344-a05311699fe0';
+        break;
+      case 'health & fitness':
+        return '0941f6f7-2b29-4ec7-94a7-98b5aec2b34a';
+        break;
+      case 'sports':
+        return '8f7ad435-5e46-4362-892e-f2550fdf0da6';
+        break;
+      case 'loungewear':
+        return '6b4b3e11-7cef-4afb-a9e9-fa8293b35201';
+        break;
+      case 'beauty':
+        return 'a4188517-0ad3-4657-8c12-4738961e0acf';
+        break;
+      case 'travel':
+        return 'babd095d-ef92-413e-8e5d-f42374b63910';
+        break;
+      case 'accessories for the cold':
+        return 'b6a5c2f0-3607-4b58-91cc-c789cf092120';
+        break;
+      case 'gadgets':
+        return 'f50e429b-d1ed-4bb1-8fb8-04b230d7c290';
+        break;
+      case 'pets':
+        return '715f7ad6-386a-42a1-9dee-9f818b4e6a78';
+        break;
+      case 'food & drink':
+        return 'abf88bc2-4b51-4cbc-a24a-b62a96461728';
+        break;
+      case 'experiences':
+        return '5f6d2a01-ac09-42cd-82e5-bf0ce354c050';
+        break;
+      case 'art & craft':
+        return 'b5354a2f-7819-4ff6-9649-b38f41512590';
+        break;
+      case 'apparel':
+        return '5619e264-24d4-4005-89a4-e23ddf640c9f';
+        break;
+      default:
+        return '';
+    }
+  }
+
+  //get encrypted giftWrapping values
+  getEncryptedSocialInterestFilters(socialInterest: string): string {
+    switch (socialInterest.toLowerCase()) {
+      case 'eco focus':
+        return 'efe15a63-d3d1-44a1-b38e-bfd584fbb3be';
+        break;
+      case 'black founder':
+        return '372272d8-7064-4239-a94c-2236af9a391d'; //done till here
+        break;
+      case 'female founder':
+        return '8e5906ca-81f3-4c61-a1c3-09b5c153ef14';
+        break;
+      case 'bipoc founder':
+        return '365e2fde-bd44-4971-8db4-23f5df836c7a';
+        break;
+      case 'b crop':
+        return '6cf74970-3d12-47d0-8dd5-a100a144996e';
+        break;
+      case 'vegan':
+        return '1cb93d44-58a0-4c38-9241-b30500d7acc3';
+        break;
+      case 'give back':
+        return '61b97c4f-109c-434f-919a-792dad402787';
+        break;
+      case 'artisan':
+        return 'cb702d5b-833f-48a6-b199-5927a7531745';
+        break;
+      case 'small business':
+        return '8d94d26a-81e6-4cec-b71b-7dfb798ed819';
+        break;
+      default:
+        return '';
+    }
+  }
+
+  //get encypted color value
+  getEncryptedColorFilters(socialInterest: string): string {
+    switch (socialInterest.toLowerCase()) {
+      case 'neutrals':
+        return 'ed028382-9a17-45f0-ae5b-68480a64a98e';
+        break;
+      case 'white':
+        return '749bd575-45d0-4b05-bdfc-78e020049844'; //done till here
+        break;
+      case 'black':
+        return '059381bc-1bea-4c0a-b1b3-3accc89978d5';
+        break;
+      case 'silver':
+        return 'c7bab18d-5d25-40f2-b149-bbd9e863eeb9';
+        break;
+      case 'gold':
+        return '621928ae-81d6-42ba-9c12-e44c7b34e98c';
+        break;
+      case 'blue':
+        return '05e18e01-a03e-4f22-99fc-bf1286748a96';
+        break;
+      case 'brown':
+        return 'b0c45d98-a51c-4189-ba3b-dfc9492d2ce8';
+        break;
+      case 'green':
+        return '4bb66245-bf9e-4df6-9ff1-c85779b962ae';
+        break;
+      case 'orange':
+        return 'e63a7721-482f-4579-91a4-3af1d23495f0';
+        break;
+      case 'pink':
+        return '0b9a41ee-e450-46d7-98a1-9752e3025a88';
+        break;
+      case 'purple':
+        return '8ba1b57d-c494-40ad-9a9f-ce4e594eb2ed';
+        break;
+      case 'red':
+        return 'd3f71588-219c-453b-90b8-b10bbf0d938c';
+        break;
+      case 'yellow':
+        return 'd45db9de-a0b2-4557-a135-7a286e107198';
+        break;
+      case 'multicolor':
+        return 'c50eb225-f561-497f-bcac-ace5afaaa8ad';
+        break;
+      default:
+        return '';
     }
   }
 
@@ -295,12 +480,17 @@ export class HomeComponent implements OnInit {
 
   constructor(private api: ApiServiceService) {
     effect(() => {
+      console.log('current page number::', this.page());
       const url = `https://api.toandfrom.com/v2/product?page=${
         this.page() + 1
       }&offset=${this.page() * 25 ? this.page() * 25 - 1 : 0}&limit=24${
         this.checkSection() ? '&segment=Kid' : ''
-      }&Gift+Wrapping=${this.encryptedGiftWrappingValue()}&minPrice=${this.sliderStartValueS()}&maxPrice=${this.sliderEndValueS()}&relationship=${this.encryptedRelationshipValue()}&occasion=${this.encryptedOccValue()}&gender=${this.encryptedGiftForValue()}&bigcommerceEnabled=${this.ourPrd()}`;
-      this.api.getProducts(url).subscribe({
+      }&order=${this.order()}&orderby=${this.orderby()}&Color+Family+%28beta%29=${this.encryptedColorFilters()}&Social+Interests+%28beta%29=${this.encryptedSocialInterestFilters()}&Gift+Wrapping=${this.encryptedGiftWrappingValue()}&Interests=${this.encryptedInterestFilters()}&minPrice=${this.sliderStartValueS()}&maxPrice=${this.sliderEndValueS()}&relationship=${this.encryptedRelationshipValue()}&occasion=${this.encryptedOccValue()}&gender=${this.encryptedGiftForValue()}&bigcommerceEnabled=${this.ourPrd()}`;
+
+      //connverting the url into an observable
+      const url$: Observable<string> = of(url);
+
+      this.api.getProducts(url$).subscribe({
         next: (response: any) => {
           // Handle successful response
           this.totalNumberOfProducts = response.total;
@@ -324,6 +514,7 @@ export class HomeComponent implements OnInit {
       this.getGiftForEncryptedValue(this.giftFor());
       this.getOccassionEncryptedValue(this.occ());
       this.getRelationshipEncryptedValue(this.relationship());
+      // this.getGiftWrappingEncryptedValue()
     });
   }
   ngOnInit(): void {
@@ -331,6 +522,7 @@ export class HomeComponent implements OnInit {
     //orignal data from  "https://api.toandfrom.com/v2/product?page=1&offset=0&limit=24"
     // this.setData(0);
   }
+
   handleSignatureWrapping(targetElem: string) {
     const closeElem: any = document.getElementById(targetElem);
     closeElem.style.display = 'inline';
@@ -340,7 +532,16 @@ export class HomeComponent implements OnInit {
     if (targetElem.includes('giftWrapping')) {
       this.giftWrapping.push(Elem.textContent);
       this.giftWrappingIds.push(targetElem);
-      this.encryptedGiftWrappingValue.set(this.giftWrapping.join(','));
+      console.log('value of giftWrapping is ::', this.giftWrapping);
+      this.encryptedGiftWrappingValue.set(
+        this.giftWrapping
+          .map((giftWrap) => this.getGiftWrappingEncryptedValue(giftWrap))
+          .join('%2C')
+      );
+      console.log(
+        'encrypted giftwarpping value::',
+        this.encryptedGiftWrappingValue()
+      );
     }
     if (
       targetElem.includes('interests') &&
@@ -348,10 +549,23 @@ export class HomeComponent implements OnInit {
     ) {
       this.interestFilters.push(Elem.textContent);
       this.interestFiltersIds.push(targetElem);
+      // console.log('my interest filters::', this.interestFilters);
+      this.encryptedInterestFilters.set(
+        this.interestFilters
+          .map((interest) => this.getEncryptedInterestFilters(interest))
+          .join('%2C')
+      );
     }
     if (targetElem.includes('social_interests')) {
       this.socialInterestsFilters.push(Elem.textContent);
       this.socialInterestFiltersIds.push(targetElem);
+      this.encryptedSocialInterestFilters.set(
+        this.socialInterestsFilters
+          .map((socialInterest) =>
+            this.getEncryptedSocialInterestFilters(socialInterest)
+          )
+          .join('%2C')
+      );
     }
   }
   //handle color click selection
@@ -371,9 +585,11 @@ export class HomeComponent implements OnInit {
         this.colorFilters.findIndex((color) => color == colorValue),
         1
       );
-      this.colorFilters.splice(
-        this.colorFiltersIds.findIndex((id) => id == targetElem),
-        1
+
+      this.encryptedColorFilters.set(
+        this.colorFilters
+          .map((interest) => this.getEncryptedColorFilters(interest))
+          .join('%2C')
       );
     }
     //if the color and the elem id does not exits in their respective arrays add them in there respective
@@ -384,6 +600,11 @@ export class HomeComponent implements OnInit {
         'rgb(255, 255, 255) 0px 0px 10px 0px, rgb(255, 0, 255) 0px 0px 10px 0px, rgb(255, 255, 0) 0px 0px 10px 0px';
       this.colorFilters.push(colorValue);
       this.colorFiltersIds.push(targetElem);
+      this.encryptedColorFilters.set(
+        this.colorFilters
+          .map((interest) => this.getEncryptedColorFilters(interest))
+          .join('%2C')
+      );
     }
   }
   closeSignatureWrapping(targetElem: string) {
@@ -398,7 +619,15 @@ export class HomeComponent implements OnInit {
       this.giftWrapping = this.giftWrapping.filter(
         (giftWrap) => giftWrap != unchosenFilterText
       );
-      this.encryptedGiftWrappingValue.set(this.giftWrapping.join(','));
+      this.encryptedGiftWrappingValue.set(
+        this.giftWrapping
+          .map((giftWrap) => this.getGiftWrappingEncryptedValue(giftWrap))
+          .join('%2C')
+      );
+      // console.log(
+      //   'after close encrypted gift wrapping::',
+      //   this.encryptedGiftWrappingValue()
+      // );
     }
     if (
       targetElem.includes('interests') &&
@@ -407,11 +636,22 @@ export class HomeComponent implements OnInit {
       this.interestFilters = this.interestFilters.filter(
         (giftWrap) => giftWrap != unchosenFilterText
       );
-      // this.numberOfInterestFiltersSelected--;
+      this.encryptedInterestFilters.set(
+        this.interestFilters
+          .map((interest) => this.getEncryptedInterestFilters(interest))
+          .join('%2C')
+      );
     }
     if (targetElem.includes('social_interests')) {
       this.socialInterestsFilters = this.socialInterestsFilters.filter(
         (giftWrap) => giftWrap != unchosenFilterText
+      );
+      this.encryptedSocialInterestFilters.set(
+        this.socialInterestsFilters
+          .map((socialInterest) =>
+            this.getEncryptedSocialInterestFilters(socialInterest)
+          )
+          .join('%2C')
       );
     }
   }
@@ -437,6 +677,7 @@ export class HomeComponent implements OnInit {
     });
     this.interestFiltersIds = [];
     this.interestFilters = [];
+    this.encryptedInterestFilters.set('');
   }
   //close all interests filters
   closeAllSocialInterestFilters() {
@@ -448,6 +689,7 @@ export class HomeComponent implements OnInit {
     });
     this.socialInterestFiltersIds = [];
     this.socialInterestsFilters = [];
+    this.encryptedSocialInterestFilters.set('');
   }
   //close all color filters
   closeAllColorFilters() {
@@ -461,6 +703,7 @@ export class HomeComponent implements OnInit {
     });
     this.colorFiltersIds = [];
     this.colorFilters = [];
+    this.encryptedColorFilters.set('');
   }
   //function to set the value of sliderStartThumb
   handleStartThumb() {
@@ -501,11 +744,17 @@ export class HomeComponent implements OnInit {
   removeRelationFilter() {
     this.relationship.set('');
   }
+  //remove sort filter
+  removeSortFilter() {
+    this.order.set('');
+    this.orderby.set('');
+  }
   //remove giftFor filter
   removeAllTheFilters() {
     this.removeGiftForFilter();
     this.removeOccassionFilter();
     this.removeRelationFilter();
+    this.removeSortFilter();
     this.removeBudgetFilter();
     this.removeGiftWrappingFilter();
     this.removeInterestsFilter();
